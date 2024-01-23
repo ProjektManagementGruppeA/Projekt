a84120e8ae1ae05150f75b5980bf1fdb79e6309e
NoraHSBO <148795720+NoraHSBO@users.noreply.github.com>
2024-01-23 14:31:17 +0100

    Update InnentürenValidierung.java

--- src/gui/innentueren/InnentürenValidierung.java
+++ src/gui/innentueren/InnentürenValidierung.java
@@ -1,21 +1,64 @@
 package gui.innentueren;
 
-public class InnentürenValidierung {
+public class InnentuerenValidierung {
 
-	public static boolean validiereGlasKlar(int i) {
-		// TODO Auto-generated method stub
-		return true;
+    // Methoden zur Überprüfung des Gebäudezustands
+    public static boolean hasDachgeschoss(boolean hasDachgeschoss) {
+        return hasDachgeschoss;    
+    }
+
+    public static boolean isObergeschoss(boolean isObergeschoss) {
+        return isObergeschoss;    
+    }
+
+    public static boolean isErdgeschoss(boolean isErdgeschoss) {
+        return isErdgeschoss;    
+    }
+
+    public static boolean isKeller(boolean isKeller) {
+        return isKeller;    
+    }
+
+    public static boolean validiereGlasKlar(int i, boolean isKeller, boolean isDachgeschoss, boolean isErdgeschoss, boolean isObergeschoss) {
+        boolean isValid = true;
+        if(isKeller)
+            isValid = isDachgeschoss ? i == 1 : i == 2;
+
+        if(isErdgeschoss)
+            isValid = i == 0 || i == 1;
+        if(isObergeschoss)
+            isValid = i == 3 || i == 4;
+
+        if(isDachgeschoss)
+            isValid = i >= 0 && i <= 2;
+        return isValid;
+    }
+    
+     public static boolean validiereGlasKlar{
+       return true;
 	}
 
-	public static boolean validiereGlasMilch(int i) {
-		// TODO Auto-generated method stub
-		return true;
-	}
+    public static boolean validiereGlasMilch(int i, boolean isKeller, boolean isDachgeschoss, boolean isErdgeschoss, boolean isObergeschoss) {
+        boolean isValid = true;
+        if(isKeller)
+            isValid = isDachgeschoss ? i == 1 : i == 2;
 
-	public static boolean validiereGarage(int i) {
-		// TODO Auto-generated method stub
-		// Weiß nicht ob da auch was validiert werden muss
-		return true;
-	}
+        if(isErdgeschoss)
+            isValid = i == 0 || i == 1;
+        if(isObergeschoss)
+            isValid = i == 3 || i == 4;
 
+        if(isDachgeschoss)
+            isValid = i >= 0 && i <= 2;
+        return isValid;
+    }
+
+       public static boolean validiereGlasMilch(int i){
+        return true;
+	  }
+
+    public static boolean validiereGarage(int i) {
+        return true;
+    }
 }
+
